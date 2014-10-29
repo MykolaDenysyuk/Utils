777db22cd298505a0611c74870900fedf9583857
Mykola Denysyuk <theshark.abc@gmail.com>
2014-10-29 19:49:27 +0200

    new file:   Extensions/UIView+SizeAndPosition

---
+++ Extensions/UIView+SizeAndPosition.h
@@ -0,0 +1,22 @@
+//
+//  UIView+SizeAndPosition.h
+//  UIView+SizeAndPosition
+//
+//  Created by Mykola Denysyuk on 10/29/14.
+//  Copyright (c) 2014 Mykola Denysyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SizeAndPosition)
+
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGFloat xOrigin;
+@property (nonatomic) CGFloat yOrigin;
+@property (nonatomic) CGSize size;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGFloat xCenter;
+@property (nonatomic) CGFloat yCenter;
+
+@end
---
+++ Extensions/UIView+SizeAndPosition.m
@@ -0,0 +1,100 @@
+//
+//  UIView+SizeAndPosition.m
+//  UIView+SizeAndPosition
+//
+//  Created by Mykola Denysyuk on 10/29/14.
+//  Copyright (c) 2014 Mykola Denysyuk. All rights reserved.
+//
+
+#import "UIView+SizeAndPosition.h"
+
+@implementation UIView (SizeAndPosition)
+
+#pragma mark - Setters:
+
+- (void)setOrigin:(CGPoint)origin
+{
+    [self setFrame:CGRectMake(origin.x, origin.y,
+                              CGRectGetWidth(self.frame),
+                              CGRectGetHeight(self.frame))];
+}
+
+- (void)setXOrigin:(CGFloat)xOrigin
+{
+    [self setOrigin:CGPointMake(xOrigin, self.yOrigin)];
+}
+
+- (void)setYOrigin:(CGFloat)yOrigin
+{
+    [self setOrigin:CGPointMake(self.xOrigin, yOrigin)];
+}
+
+- (void)setSize:(CGSize)size
+{
+    [self setFrame:CGRectMake(self.origin.x, self.origin.y,
+                              size.width, size.height)];
+}
+
+- (void)setWidth:(CGFloat)width
+{
+    [self setSize:CGSizeMake(width, self.height)];
+}
+
+- (void)setHeight:(CGFloat)height
+{
+    [self setHeight:CGSizeMake(self.width, height)];
+}
+
+- (void)setXCenter:(CGFloat)xCenter
+{
+    [self setCenter:CGPointMake(xCenter, self.yCenter)];
+}
+
+- (void)setYCenter:(CGFloat)yCenter
+{
+    [self setCenter:CGPointMake(self.xCenter, yCenter)];
+}
+
+#pragma mark - Getters:
+
+- (CGPoint)origin
+{
+    return self.frame.origin;
+}
+
+- (CGFloat)xOrigin
+{
+    return self.origin.x;
+}
+
+- (CGFloat)yOrigin
+{
+    return self.origin.y;
+}
+
+- (CGSize)size
+{
+    return self.frame.size;
+}
+
+- (CGFloat)width
+{
+    return self.size.width;
+}
+
+- (CGFloat)height
+{
+    return self.size.height;
+}
+
+- (CGFloat)xCenter
+{
+    return self.center.x;
+}
+
+- (CGFloat)yCenter
+{
+    return self.center.y;
+}
+
+@end
