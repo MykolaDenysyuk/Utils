560b904eaa36b321ef9b1326af435181470cdf29
Mykola Denysyuk <theshark.abc@gmail.com>
2015-05-07 16:49:05 +0300

    adjusted CollectionDatasourceDelegate

--- CustomClasses/CollectionViewHelper/CollectionDatasourceDelegate.m
+++ CustomClasses/CollectionViewHelper/CollectionDatasourceDelegate.m
@@ -8,6 +8,8 @@
 
 #import "CollectionDatasourceDelegate.h"
 
+NSString * const CollectionDefaultReusableViewIdentifier = @"reusableViewId";
+
 @interface CollectionDatasourceDelegate ()
 {
     IBOutlet UICollectionView * _nibCollectionView;
@@ -46,6 +48,10 @@
     _collectionView = collectionView;
     _collectionView.delegate = self;
     _collectionView.dataSource = self;
+    
+    [_collectionView registerClass:[UICollectionReusableView class]
+        forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+               withReuseIdentifier:CollectionDefaultReusableViewIdentifier];
 }
 
 #pragma mark - Accessors:
@@ -69,11 +75,54 @@
 
 #pragma mark - UICollectionView:
 
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    if (_numberOfSectionsBlock) {
+        return _numberOfSectionsBlock(collectionView);
+    }
+    return 1;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    if (_returnSupplementaryViewBlock) {
+        return _returnSupplementaryViewBlock(collectionView, kind, indexPath);
+    }
+    
+    UICollectionReusableView* view = [collectionView dequeueReusableSupplementaryViewOfKind:kind
+                                                                        withReuseIdentifier:CollectionDefaultReusableViewIdentifier
+                                                                               forIndexPath:indexPath];
+    if (_configureSupplementaryViewBlock) {
+        _configureSupplementaryViewBlock(collectionView, view, kind, indexPath);
+    }
+    return view;
+}
+
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
     if (self.numberOfItemsBlock)
         return self.numberOfItemsBlock(collectionView, section);
     return self.items.count;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (_getItemSizeBlock)
+        return _getItemSizeBlock(collectionView, collectionViewLayout, indexPath);
+    return [(UICollectionViewFlowLayout*)collectionViewLayout itemSize];
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    if (_minimumLineSpacingBlock)
+        return _minimumLineSpacingBlock(collectionView, collectionViewLayout, section);
+    return [(UICollectionViewFlowLayout*)collectionViewLayout minimumLineSpacing];
+}
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    if (_minimumInteritemSpacingBlock)
+        return _minimumInteritemSpacingBlock(collectionView, collectionViewLayout, section);
+    return [(UICollectionViewFlowLayout*)collectionViewLayout minimumInteritemSpacing];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
