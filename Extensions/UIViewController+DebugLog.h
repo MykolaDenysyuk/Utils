6ce8ec2c4156b219110f38a7ed99bad4585455ba
Mykola Denysyuk <theshark.abc@gmail.com>
2015-12-15 13:53:09 +0200

    UIViewController+DebugLog is added.

---
+++ Extensions/UIViewController+DebugLog.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+DebugLog.h
+//  Utils
+//
+//  Created by Mykola Denysyuk on 12/15/15.
+//  Copyright © 2015 Mykola Denysyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (DebugLog)
+
+@end
